70b0e31131a680c7aa6ea6a7966d99553c909174
Simon Dao <SimonNDao12@gmail.com>
2024-01-01 19:44:35 -0800

    removed boiler plate

---
+++ src/containers/Todo.jsx
@@ -0,0 +1,13 @@
+import './Todo.css';
+import { useState } from 'react';
+
+function Todo() {
+  
+  const [todos, setTodos] = useState([])
+  
+  return (
+    <div>todo list!!!!</div>
+  );
+}
+
+export default Todo;
